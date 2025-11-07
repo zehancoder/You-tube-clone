67064bfbd8f11b2084b7bcc3b9622656ea59c38e
zehancoder <zehanjh93@gmail.com>
2025-11-07 21:39:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-4xl font-bold '>Hello Developers</h2>
+    </>
+  )
+}
+
+export default App
